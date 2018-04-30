475b1d9c5fe9a12f0ad7cb9b9c835decd4b0235c
haueyne <rhaplanca.maoh@gmail.com>
2018-04-30 20:05:34 +0900

    whitenoise使用

--- tncProject/wsgi.py
+++ tncProject/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
+# from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tncProject.settings")
 
-application = Cling(get_wsgi_application())
-# application = DjangoWhiteNoise(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
