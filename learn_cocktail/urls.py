2189a647fd06c55e93baf333575e4fb08a4a6e14
haueyne <rhaplanca.maoh@gmail.com>
2018-04-30 16:52:08 +0900

    URL設定

--- learn_cocktail/urls.py
+++ learn_cocktail/urls.py
@@ -3,8 +3,7 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='learn_cocktail/index.html'), name='top'),
-    # path('', views.top, name='top'),
-    # path('question/', views.question, name='question'),
-    # path('answer/', views.answer, name='answer'),
+    path('', views.top, name='top'),
+    path('question/', views.question, name='question'),
+    path('answer/', views.answer, name='answer'),
 ]
