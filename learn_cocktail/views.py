e196ee3b9e37ec8a8ab48cad95e41f305410b7a2
haueyne <38074882+haueyne@users.noreply.github.com>
2018-05-01 16:09:46 +0900

    指定モデル要素ランダム取得関数作成

--- learn_cocktail/views.py
+++ learn_cocktail/views.py
@@ -1,9 +1,18 @@
+from random import randint
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models.aggregates import Count
 from .forms import QuestionForm
 from .models import Cocktail
 
 # Create your views here.
+
+
+def _get_one_item_randomly(model):
+    """指定モデルからランダムに１件取得する"""
+    count = model.objects.aggregate(count=Count('id'))['count']
+    random_index = randint(0, count - 1)
+    return model.objects.all()[random_index]
 
 
 def top(request):
