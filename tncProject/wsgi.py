68e64db7c07001174a14bd94038070381509a28b
haueyne <rhaplanca.maoh@gmail.com>
2018-05-07 21:08:10 +0900

    Awake設定追加

--- tncProject/wsgi.py
+++ tncProject/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import threading
+import requests
+import time
 
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
@@ -15,3 +18,22 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tncProject.settings")
 
 application = DjangoWhiteNoise(get_wsgi_application())
+
+
+def awake():
+    minute = 60
+    awake_url = getattr(os.environ, 'AWAKE_URL', False)
+
+    while awake_url:
+        try:
+            print('Start Awaking')
+            requests.get(awake_url)
+            print("End")
+        except Exception:
+            print("error")
+
+        time.sleep(25 * minute)
+
+
+t = threading.Thread(target=awake)
+t.start()
